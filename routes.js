34dbd2fe88d8213f6634f3cddae3272efb99cb1e
David Pio <dpio222@gmail.com>
2012-07-08 14:17:17 -0700

    Abstract image URI construction

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+var resources = require('./lib/resources');
 var tracks = require('./data/tracks').Tracks;
 
 module.exports = function(app){
@@ -5,12 +6,13 @@ module.exports = function(app){
     // if a track is supplied and exists
     if (req.params.track && tracks[req.params.track]) {
       var track = tracks[req.params.track];
-      
+      var og_image = resources.getImagePath(req, track.image);
+
       res.render('track.ejs', {	  
         layout: false,
         app_id: process.env.FACEBOOK_APP_ID,
 	og_url: 'https://' + req.headers['host'] + '/track/' + req.params.track,
-	og_image: 'https://' + req.headers['host'] + '/images/' + track.image,
+	og_image: og_image,
 	track_id: req.params.track,
 	track: track
       });
