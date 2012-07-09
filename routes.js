314ba32e6eee11052bc20e4755a53f5d8e6867e3
David Pio <dpio222@gmail.com>
2012-07-09 16:35:43 -0700

    add lap get/set db method and lap get endpoint

--- routes.js
+++ routes.js
@@ -36,38 +36,28 @@ module.exports = function(app){
   });
 
   app.get('/laps/:lap', function(req, res) {
-    db.getLapsCollection(function(lc) {
-      // if a lap is supplied and exists
-      renderOr404(res,
-        req.params.lap && true, // change this later
-        'Lap not found',
-        function() {
-          var lap = {
-            name: 'foo',
-	    description: 'bar',
-	    time: 1,
-	    top_speed: 2,
-	    track: 'thunderhill',
-	    coordinates: [
-	      { latitude: 39.543820, longitude: -122.328909 },
-	      { latitude: 39.544581, longitude: -122.329832 }
-	    ]
-	  };
-
-	  var og_url = resources.getLapURI(req, req.params.lap);
-	  var og_image = resources.getImageURI(req, tracks[lap.track].image);
-	  var track_url = resources.getTrackURI(req, lap.track);
+    if (req.params.lap) {
+      db.Laps.getLap(req.params.lap, function(lap) {
+        if (lap) {
+          var og_url = resources.getLapURI(req, req.params.lap);
+          var og_image = resources.getImageURI(req, tracks[lap.track].image);
+          var track_url = resources.getTrackURI(req, lap.track);
 
           res.render('lap.ejs', {
             layout: false,
-	    app_id: process.env.FACEBOOK_APP_ID,
-	    og_url: og_url,
-	    og_image: og_image,
+            app_id: process.env.FACEBOOK_APP_ID,
+            og_url: og_url,
+            og_image: og_image,
             lap: lap,
-	    track_url: track_url
-	  });
+            track_url: track_url
+          });
+	} else {
+	  res.send('Lap not found', 404);
+	}
       });
-    });
+    } else {
+      res.send('Lap not found', 404);
+    }
   });
 
   //other routes..
